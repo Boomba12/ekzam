7c297e7e09433ce64014882418e3d23188050716
Boomba12 <93035011+Boomba12@users.noreply.github.com>
2021-10-24 01:11:36 +0300

    первичная верстка, первичный внос логики

--- local/components/biv/ekzam/class.php
+++ local/components/biv/ekzam/class.php
@@ -1,18 +1,126 @@
 <?php
 
+use Bitrix\Main\Loader;
+use Bitrix\Highloadblock; 
+use Bitrix\Main\Entity;
+
 class CEkzam extends CBitrixComponent
 {
     public function onPrepareComponentParams($arParams)
     {
         $result = [
-            "CACHE_TYPE" => $arParams["CACHE_TYPE"],
-            "CACHE_TIME" => isset($arParams["CACHE_TIME"]) ?$arParams["CACHE_TIME"]: 36000,
+            'CACHE_TYPE' => $arParams['CACHE_TYPE'],
+            'CACHE_TIME' => isset($arParams['CACHE_TIME']) ?$arParams['CACHE_TIME']: 36000,
         ];
         return $result;
     }
     public function executeComponent()
     {
+        $arTask = self::getTask();
+        $arState = self::getState();
+        
+        $arResult['EXECUTORS'] = $arExec = self::getExecutor();
+        $arResult['TASKS'] = self::builder($arTask,$arExec,$arState);
 
         $this->includeComponentTemplate();
+    }
+
+    private function builder($arTask,$arExec,$arState)
+    {
+        foreach ($arTask as $key => $task) {
+            $build[$key] = [
+                'NAME' => $task['NAME'],
+                'EXECUTOR' => $arExec[$task['EXECUTOR']]['NAME'],
+                'STATE' => $arState[$task['STATE']]['NAME'],
+                'DESCRIPTION' => $task['DESCRIPTION']
+            ];
+        }
+
+        return $build;
+    }
+
+    private function getTask()
+    {
+        Loader::includeModule('highloadblock');
+
+        $hlbl = 5; 
+        $hlblock = Highloadblock\HighloadBlockTable::getById($hlbl)->fetch(); 
+
+        $entity = Highloadblock\HighloadBlockTable::compileEntity($hlblock); 
+        $entity_data_class = $entity->getDataClass(); 
+
+        $dbResult = $entity_data_class::getList(
+            [
+                'select' => [
+                    '*'
+                ],
+                'order' => [
+                    'ID' => 'ASC'
+                ]
+            ]
+        );
+        while($element = $dbResult->Fetch()){
+            $arData[$element['ID']] = [
+                'NAME' => $element['US_TASK_NAME'],
+                'EXECUTOR' => $element['US_TASK_EXECUTOR'],
+                'STATE' => $element['US_TASK_STATE'],
+                'DESCRIPTION' => $element['US_TASK_DESCRIPTION']
+            ];
+        }
+
+        return $arData;
+    }
+
+    private function getExecutor()
+    {
+        Loader::includeModule('highloadblock');
+
+        $hlbl = 6; 
+        $hlblock = Highloadblock\HighloadBlockTable::getById($hlbl)->fetch(); 
+        $entity = Highloadblock\HighloadBlockTable::compileEntity($hlblock); 
+        $entity_data_class = $entity->getDataClass(); 
+        $dbResult = $entity_data_class::getList(
+            [
+                'select' => [
+                    '*'
+                ],
+                'order' => [
+                    'ID' => 'ASC'
+                ]
+            ]
+        );
+        while($element = $dbResult->Fetch()){
+            $arData[$element['ID']] = [
+                'NAME' => $element['US_EXECUTOR_NAME'],
+                'POSITION' => $element['US_EXECUTOR_POSITION'],
+            ];
+        }
+        return $arData;
+    }
+
+    private function getState()
+    {
+        Loader::includeModule('highloadblock');
+
+        $hlbl = 7; 
+        $hlblock = Highloadblock\HighloadBlockTable::getById($hlbl)->fetch(); 
+        $entity = Highloadblock\HighloadBlockTable::compileEntity($hlblock); 
+        $entity_data_class = $entity->getDataClass(); 
+        $dbResult = $entity_data_class::getList(
+            [
+                'select' => [
+                    '*'
+                ],
+                'order' => [
+                    'ID' => 'ASC'
+                ]
+            ]
+        );
+        while($element = $dbResult->Fetch()){
+            $arData[$element['ID']] = [
+                'NAME' => $element['US_STATE_NAME'],
+            ];
+        }
+        return $arData;
     }
 }
--- local/components/biv/ekzam/templates/.default/template.php
+++ local/components/biv/ekzam/templates/.default/template.php
@@ -2,13 +2,38 @@
     $this->addExternalCss('styles.css');
 ?>
 
-
 <div class="tabs">
-<input type="radio" name="tab-btn" id="tab-btn-1" value="" checked>
-<label for="tab-btn-1">Задачи</label>
-<input type="radio" name="tab-btn" id="tab-btn-2" value="">
-<label for="tab-btn-2">Исполнители</label>
+    <input type="radio" name="tab-btn" id="tab-btn-1" value="" checked>
+    <label for="tab-btn-1">Задачи</label>
+    <input type="radio" name="tab-btn" id="tab-btn-2" value="">
+    <label for="tab-btn-2">Исполнители</label>
 
-    <div id="content-1">Задачи</div>
-    <div id="content-2">Исполнители</div>
+    <div id="content-1">
+        <?foreach ($arResult['TASKS'] as $key => $task):?>
+            <div class='row'>
+                <span class='task-id'><?=$key?></span>
+                <span class='task-name'><?=$task['NAME']?></span>
+                <span class='task-state'><?=$task['STATE']?></span>
+                <span class='task-executor'><?=$task['EXECUTOR']?></span>
+                <span class='task-descr'><?=$task['DESCRIPTION']?></span>
+                <span class='task-buttons'>
+                    <span>Удалить</span>
+                    <span>Редактировать</span>
+                </span>
+            </div>
+        <?endforeach;?>
+    </div>
+    <div id="content-2">
+        <?foreach ($arResult['EXECUTOR'] as $key => $exec):?>
+            <div class='row'>
+                <span class='exec-id'><?=$key?></span>
+                <span class='exec-name'><?=$exec['NAME']?></span>
+                <span class='exec-position'><?=$exec['POSITION']?></span>
+                <span class='exec-buttons'>
+                    <span>Удалить</span>
+                    <span>Редактировать</span>
+                </span>
+            </div>
+        <?endforeach;?>
+    </div>
 </div>
